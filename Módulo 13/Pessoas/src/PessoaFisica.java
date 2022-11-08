5a5d472d5a33994578adddd4ad02c03282d412d7
Lucas-da-Silva-Aleixo <lucasdasilvaaleixo123@gmail.com>
2022-11-08 18:14:29 -0300

    test

---
+++ Módulo 13/Pessoas/src/PessoaFisica.java
@@ -0,0 +1,10 @@
+public class PessoaFisica extends Pessoas {
+	
+	public static void getName() {
+		String name = "José";
+	}
+	
+	public static void getCPF() {
+		long cpf = 378274978;
+	}
+}
---
+++ Módulo 13/Pessoas/src/PessoaJuridica.java
@@ -0,0 +1,11 @@
+public class PessoaJuridica extends Pessoas {
+	
+	public static void getCompanyName() {
+		String companyName = "Apple";
+	}
+	
+	public static void getCNPJ() {
+		long cnpj = 432423432;
+	}
+	
+}
